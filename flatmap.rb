a357a33459dbb4f80bd95ccc78e97557e1f0b4b4
chrsearl <chris.searle@finn.no>
2013-09-23 12:32:11 +0200

    Convert for url_for and sinatra/json

--- flatmap.rb
+++ flatmap.rb
@@ -1,17 +1,14 @@
 require 'sinatra'
-require 'json'
+require 'sinatra/json'
+require 'sinatra/url_for'
 require 'mongo'
 
 include Mongo
 
-if (settings.environment == :development)
-  HOST = "http://localhost:9292"
-
+if settings.environment == :development
   client = MongoClient.new('localhost', '27017')
   db = client.db('test')
 else
-  HOST = "http://flatmap-ems.herokuapp.com"
-
   mongo_uri = ENV['MONGOLAB_URI']
   db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
   client = MongoClient.from_uri(mongo_uri)
@@ -23,77 +20,87 @@ get '/static/EMS-Config.plist' do
   send_file File.join(settings.root, 'static/EMS-Config.plist')
 end
 
+get '/events/:slug/sessions' do
+  send_collection('/', {})
+end
+
+get '/events/:slug/slots' do
+  send_collection('/', {})
+end
+
+get '/events/:slug/rooms' do
+  send_collection('/', {})
+end
+
+get '/events/:slug' do
+  events(db["events"].find("slug" => params[:slug]))
+end
+
 get '/events' do
-  data = {
-    :collection => {
-      :version => "1.0",
-      :href => "#{HOST}/events",
-      :items => []
-    }
-  }
-  
-  events = db["events"]
-  
-  events.find.each do |event|
-    data[:collection][:items] << {
-      :href => "#{HOST}/events/#{event['slug']}",
-      :name => event["name"],
-      :data => [
-        {
-          :name => "name",
-          :value => event["name"]
-        },
-        {
-          :name => "slug",
-          :value => event["slug"]
-        },
-        {
-          :name => "venue",
-          :value => event["venue"]
-        }
-      ],
-      :links => [
-        {
-            :href => "#{HOST}/events/#{event['slug']}/sessions",
-            :rel => "session collection",
-            :count => 0 # session count
-        },
-        {
-            :href => "#{HOST}/events/#{event['slug']}/slots",
-            :rel => "slot collection"
-        },
-        {
-            :href => "#{HOST}/events/#{event['slug']}/slots",
-            :rel => "room collection"
-        }
-      ]
-    }
-  end
-  
-  
-  send_data data
+  events(db["events"])
 end
 
 get '/' do
-  data = {
-    :collection => {
-      :version => "1.0",
-      :href => "#{HOST}/",
-      :links => [
-        {
-          :href => "#{HOST}/events",
-          :rel => "event collection",
-        }
-      ]
+  send_collection("/",
+                  {:links =>
+                       [
+                           {
+                               :href => url_for("/events", :full),
+                               :rel => "event collection",
+                           }
+                       ]
+                  })
+end
+
+def events(events)
+  data = {:items => []}
+
+  events.find.each do |event|
+    data[:items] << {
+        :href => url_for("/events/#{event['slug']}", :full),
+        :name => event["name"],
+        :data => [
+            {
+                :name => "name",
+                :value => event["name"]
+            },
+            {
+                :name => "slug",
+                :value => event["slug"]
+            },
+            {
+                :name => "venue",
+                :value => event["venue"]
+            }
+        ],
+        :links => [
+            {
+                :href => url_for("/events/#{event['slug']}/sessions", :full),
+                :rel => "session collection",
+                :count => 0 # session count
+            },
+            {
+                :href => url_for("/events/#{event['slug']}/slots", :full),
+                :rel => "slot collection"
+            },
+            {
+                :href => url_for("/events/#{event['slug']}/rooms", :full),
+                :rel => "room collection"
+            }
+        ]
     }
-  }
-  
-  send_data data
+  end
+
+
+  send_collection url_for("/events", :full), data
 end
 
 
-def send_data(data)
+def send_collection(href, data)
   content_type 'application/vnd.collection+json', :charset => 'utf-8'
-  
-  data.to_json
+
+  data[:version] = "1.0"
+  data[:href] = url_for(href, :full)
+
+  json ({:collection => data})
 end
